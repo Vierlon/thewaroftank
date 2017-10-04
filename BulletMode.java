71fedcfeef1a8b2329f3b54db355fbeeeb3c207a
WuYaoLong <32410462+Vierlon@users.noreply.github.com>
2017-10-04 21:10:18 +0800

    BulletMode.java
    子弹模式枚举(目前未实现)

---
+++ BulletMode.java
@@ -0,0 +1,16 @@
+package thewaroftank.program.enums;
+/**
+ * 子弹模式枚举
+ * <li>PLAIN - 普通
+ * <li>PIERCED - 穿透
+ * <li>THREE_FIRE - 三连发
+ * <li>THREE_SPLIT - 三分裂
+ * @author Yun-Long
+ *
+ */
+public enum BulletMode {
+	PLAIN, //普通
+	PIERCED, //穿透
+	THREE_FIRE, //三连发
+	THREE_SPLIT; //三分裂
+}
