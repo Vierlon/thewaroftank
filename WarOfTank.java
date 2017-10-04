a74001102804a1146c34e64260c23e4bda4ca282
WuYaoLong <32410462+Vierlon@users.noreply.github.com>
2017-10-04 19:50:10 +0800

    WarOfTank.java
    main主方法文件,程序启动入口.

---
+++ WarOfTank.java
@@ -0,0 +1,19 @@
+package thewaroftank;
+
+import thewaroftank.gui.GameFrame;
+
+/**
+ * 游戏启动入口
+ * @author Yun-Long
+ *
+ */
+public class WarOfTank {
+
+	public static void main(String[] args) {
+		
+		//构建并显示游戏窗口
+		GameFrame.getGameFrame().setVisible(true);
+		
+	}
+
+}
